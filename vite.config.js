908ccaaf60afea83d3604bc66da0298a1f22eb95
Hicham Akil <hichamakil2018@gmail.com>
2026-06-03 11:03:02 +0100

    fix: correct setupFiles path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.js"],
+    setupFiles: ["./src/tests/setup.js"],
     css: false,
     coverage: {
       provider: "v8",
